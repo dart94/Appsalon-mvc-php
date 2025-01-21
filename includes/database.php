d40737d6a7e0d7956ea0f235102430e80b452687
dart94 <167586944+dart94@users.noreply.github.com>
2025-01-21 10:11:37 -0700

    fix: update environment variable names and add database port

--- includes/database.php
+++ includes/database.php
@@ -5,10 +5,12 @@ $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
 $db = mysqli_connect(
-    $_ENV['BD_HOST'],
-    $_ENV['BD_USER'],
-    $_ENV['BD_PASS'],
-    $_ENV['BD_NAME']
+    $_ENV['DB_HOST'],
+    $_ENV['DB_USER'],
+    $_ENV['DB_PASS'],
+    $_ENV['DB_NAME'],
+    $_ENV['DB_PORT']
+
 );
 
 $db->set_charset('utf8');
